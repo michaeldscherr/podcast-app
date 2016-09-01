7015fce6a84be001b62f6b10b6e575a84d741289
Michael Scherr <mikescherr@gmail.com>
2016-09-01 17:55:20 -0400

    works on SearchBar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -23,6 +23,45 @@
             </div>
         </div>
     </section>
+    <section class="section" v-show="results.data">
+        <table class="table  is-striped">
+            <thead>
+                <tr>
+                    <th v-for="column of results.columns">
+                        {{ column.label }}
+                    </th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <th v-for="column of results.columns">
+                        {{ column.label }}
+                    </th>
+                </tr>
+            </tfoot>
+            <tbody>
+                <tr v-for="row of results.data" track-by="collectionId">
+                    <td>
+                        <img
+                            v-bind:src="row.artworkUrl100"
+                            alt="{{ row.collectionName }}"
+                            width="50"
+                        />
+                    </td>
+                    <td>
+                        <a href="{{ row.artistViewUrl }}">
+                            {{ row.collectionName }}
+                        </a>
+                    </td>
+                    <td>
+                        <template v-for="genre of row.genres | limitBy 2">
+                            {{ genre }}<span v-if="$index === 0">, </span>
+                        </template>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
 </template>
 
 <script>
@@ -33,27 +72,39 @@
                     uri: 'https://itunes.apple.com/search',
                     params: {
                         entity: 'podcast',
-                        term: ''
+                        term: '',
+                        callback: ''
                     }
                 },
                 title: 'Search',
                 placeholder: 'This American Life',
-                results: []
+                results: {
+                    columns: [
+                        { key: 'artworkUrl100', label: 'Artwork' },
+                        { key: 'collectionName', label: 'Title' },
+                        { key: 'genres', label: 'Genres' },
+                        { key: 'artistViewUrl', label: 'iTunes URL' },
+                        { key: 'feedUrl', label: 'Feed URL' }
+                    ],
+                    data: []
+                }
             }
         },
         methods: {
             search() {
-                this.$http.get(this.query.uri, {
+                this.$http.jsonp(this.query.uri, {
                     params: this.query.params,
                     before(request) {
+                        // abort previous request, if exists
                         if (this.previousRequest) {
                             this.previousRequest.abort();
                         }
+                        // set previous request on Vue instance
                         this.previousRequest = request;
                     }
-                }).then((response) => {
-                    console.log(response);
-                    this.results = response;
+                })
+                .then((response) => {
+                    this.results.data = response.data.results;
                 }, (response) => {
                     console.log('error', response);
                 });
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import VueResource from 'vue-resource';
 import App from './App.vue';
 
 Vue.use(VueResource);
-Vue.http.options.xhr = { withCredentials: true };
 
 /* eslint-disable no-new */
 new Vue({
