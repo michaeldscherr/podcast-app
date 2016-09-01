8c72b1acd04badb44e29978815d3e476c46eb55f
Michael Scherr <mikescherr@gmail.com>
2016-09-01 10:09:51 -0400

    adds vue-resource package

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import App from './App.vue';
+
+Vue.use(VueResource);
+Vue.http.options.xhr = { withCredentials: true };
 
 /* eslint-disable no-new */
 new Vue({
-  el: 'body',
-  components: { App }
-})
+    el: 'body',
+    components: { App },
+});
